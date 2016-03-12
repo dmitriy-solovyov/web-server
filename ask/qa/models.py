13f2c91fb935d0a95f2246db1924f8ebc6219b9a
Dima <Dima@MacBook-Pro-mac.local>
2016-03-12 23:13:31 +0200

    ДОбавлены настройки для бд

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib import auth.models.User
 
-# Create your models here.
+class Question(models.Model):
+	title = models.CharField(max_length=255)
+	text = models.TextField()
+	added_at = models.DateTimeField(blank=True)
+	rating = models.IntegerField(10)
+	author = models.ForeignKey(User)
+	likes = models.ManyToManyField(Likes)
+
+class Answer(models.Model):
+	text = models.TextField()
+	added_at = models.DateTimeField(blank=True)
+	question = models.ForeignKey(Question)
+	author = models.ForeignKey(User)
+		
