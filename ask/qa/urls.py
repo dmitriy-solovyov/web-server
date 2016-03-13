943c82cc70dfabece1c3ae1217f09d054dda95a5
Dima <Dima@MacBook-Pro-mac.local>
2016-03-14 01:32:04 +0200

    URL = /?page=2
    Главная страница. Список "новых" вопросов. Т.е. последний заданный вопрос - первый в списке. На этой странице должна работать пагинация. Номер страницы указывается в GET параметре page.  На страницу выводится по 10 вопросов. В списке вопросов должны выводится заголовки (title) вопросов и ссылки на страницы отдельных вопросов.
    
    URL = /popular/?page=3
    
    Cписок "популярных" вопросов. Сортировка по убыванию поля rating. На этой странице должна работать пагинация. Номер страницы указывается в GET параметре page.  На страницу выводится по 10 вопросов. В списке вопросов должны выводится заголовки (title) вопросов и ссылки на страницы отдельных вопросов.
    
    URL = /question/5/
    
    Страница одного вопроса. На этой странице должны выводится заголовок (title), текст (text) вопроса и все ответы на данный вопрос, без пагинации.  В случае неправильного id вопроса view должна возвращать 404.

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     url(r'^$', views.question_list_all),
     url(r'^login/', views.test),
     url(r'^signup/', views.test),
-    url(r'^question/[0-9]{1,}/', views.test),
+    url(r'^question/(?P<id>\d+)/', views.question_with_answers),
     url(r'^ask/', views.test),
-    url(r'^popular/', views.test),
+    url(r'^popular/', views.popular_questions),
     url(r'^new/', views.test),
 ]
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from django.http import HttpResponse
+from qa.models import Question
+from qa.models import Answer
+from django.http import Http404
 
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
@@ -13,6 +16,31 @@ def question_list_all(request):
 	paginator.baseurl = '/?page='
 	page = paginator.page(page)
 	return render(request, 'all_questions.html' , {
-		questions : page.object_list
-		paginator : paginator, page: page,
+		'questions' : page.object_list,
+		'paginator' : paginator, 
+		'page': page,
 	})
+
+def popular_questions(request):
+	questions = Question.objects.order_by('-rating')
+	limit = request.GET.get('limit', 10)
+	page = request.GET.get('page', 1)
+	paginator = Paginator(questions, limit)
+	paginator.baseurl = '/popular/?page='
+	page = paginator.page(page)
+	return render(request, 'popular_questions.html' , {
+		'questions' : page.object_list,
+		'paginator' : paginator, 
+		'page': page,
+	})
+
+def question_with_answers(request, id):
+	try:
+		question = Question.objects.get(id=id)
+	except Question.DoesNotExist:
+		raise Http404
+	answers = Answer.objects.get(question_id=id)
+	return render(request, 'question.html' , {
+		'question' : question,
+		'answers' : answers,
+	}) 
