a4cbb2e1b4751119cd07fefe95eb6dd40a418a09
Dima <Dima@MacBook-Pro-mac.local>
2016-03-17 03:16:52 +0200

    fix

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -1,30 +1,29 @@
 from django import forms
+from django.forms import ModelForm
 from qa.models import Question
 from qa.models import Answer
 from django.contrib.auth.models import User
-from django.forms import ModelForm
 
 class AskForm(ModelForm):
-        class Meta:
-                model = Question
-                exclude = ['added_at','rating','author','likes']
-
+	class Meta:
+        	model = Question
+        	exclude = ['added_at','rating','author','likes']
 
 class AnswerForm(forms.Form):
-        text = forms.CharField(widget=forms.Textarea)
-#       question = forms.IntegerField(widget=forms.HiddenInput)
-        question = forms.IntegerField()
+	text = forms.CharField(widget=forms.Textarea)
+#	question = forms.IntegerField(widget=forms.HiddenInput)
+	question = forms.IntegerField()
 
-        def clean(self):
-                self.cleaned_data = super(AnswerForm, self).clean()
+	def clean(self):
+		self.cleaned_data = super(AnswerForm, self).clean()
                 return self.cleaned_data
 
-        def save(self,request):
-                question = Question.objects.get(id=self.cleaned_data['question'])
-        #       self.cleaned_data['question_id'] = question.id
-                self.cleaned_data['question']= question
-                self.cleaned_data['author_id'] = request.user.id
-                return Answer.objects.create(**self.cleaned_data)
+	def save(self,request):
+		question = Question.objects.get(id=self.cleaned_data['question'])
+	#	self.cleaned_data['question_id'] = question.id
+		self.cleaned_data['question']= question
+		self.cleaned_data['author_id'] = request.user.id
+		return Answer.objects.create(**self.cleaned_data)
 
 class SignUpForm(forms.Form):
 	username = forms.CharField()
@@ -45,4 +44,3 @@ class LoginForm(forms.Form):
 	def clean(self):
 		self.cleaned_data = super(LoginForm, self).clean()
 		return self.cleaned_data
-
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -13,7 +13,8 @@ def test(request, *args, **kwargs):
     return HttpResponse('OK')
 
 def question_list_all(request):
-	questions = Question.objects.order_by('-id')
+	questions = Question.objects.all()
+	questions = questions.order_by('-id')
 	limit = request.GET.get('limit', 10)
 	page = request.GET.get('page', 1)
 	paginator = Paginator(questions, limit)
@@ -21,8 +22,7 @@ def question_list_all(request):
 	page = paginator.page(page)
 	return render(request, 'all_questions.html' , {
 		'questions' : page.object_list,
-		'paginator' : paginator, 
-		'page': page,
+		'paginator' : paginator, 'page' : page,
 	})
 
 def popular_questions(request):
@@ -39,44 +39,45 @@ def popular_questions(request):
 	})
 
 def question_with_answers(request, id):
-	form = AnswerForm(initial={'question': id})
-	try:
-		question = Question.objects.get(id=id)
-	except Question.DoesNotExist:
-		raise Http404
-	answers = Answer.objects.filter(question_id=id)
-	return render(request, 'question.html' , {
-		'question' : question,
-		'answers' : answers,
-	}) 
+		form = AnswerForm(initial={'question': id})
+		try:
+			question = Question.objects.get(id=id)
+		except Question.DoesNotExist:
+			raise Http404
+		answers = Answer.objects.filter(question_id=id)
+		return render(request, 'question.html' , {
+			'question' : question,
+			'answers' : answers,
+			'form' : form,
+		}) 
 
 def question_add(request):
-        if request.method == "POST":
-                form = AskForm(request.POST)
-                if form.is_valid():
-                        question = form.save(commit=False)
-                        question.author_id = request.user.id
-                        question.rating = '1'
-                        question.save()
-                        url = reverse('question-with-answers', args=(question.id,))
-                        return HttpResponseRedirect(url)
-
-        else:
-                form = AskForm()
-        return render(request, 'question_add.html', {
-                'form' : form,
-        })
+	if request.method == "POST":
+		form = AskForm(request.POST)
+		if form.is_valid():
+			question = form.save(commit=False)
+			question.author_id = request.user.id
+			question.rating = '1'
+			question.save()
+			url = reverse('question-with-answers', args=(question.id,))
+			return HttpResponseRedirect(url)
+			
+	else:
+		form = AskForm()
+	return render(request, 'question_add.html', {
+		'form' : form,
+	}) 
 
 def answer_add(request):
-         if request.method == "POST":
-                 form = AnswerForm(request.POST)
-                 if form.is_valid():
-                         answer = form.save(request)
-                         url = reverse('question-with-answers', args=(answer.question.id,))
-                         return HttpResponseRedirect(url)
-         else:
-                form = AnswerForm()
-         return HttpResponseRedirect('/')
+	 if request.method == "POST":
+		 form = AnswerForm(request.POST)
+		 if form.is_valid():
+			 answer = form.save(request)	 
+			 url = reverse('question-with-answers', args=(answer.question.id,))
+			 return HttpResponseRedirect(url)
+	 else:
+		form = AnswerForm()
+	 return HttpResponseRedirect('/')
 
 
 def signup(request):
@@ -84,17 +85,17 @@ def signup(request):
 		form = SignUpForm(request.POST)
 		username = request.POST.get('username')
 		email = request.POST.get('email')
-        password = request.POST.get('password')
-        if form.is_valid():
-        	new_user = form.save()
-        	user = authenticate(username=username, password=password)
-        	if user is not None:
-        		login(request, user)
-            return HttpResponseRedirect('/')
-    else:
-    	form = SignUpForm()
+        	password = request.POST.get('password')
+        	if form.is_valid():
+        		new_user = form.save()
+        		user = authenticate(username=username, password=password)
+        		if user is not None:
+        			login(request, user)
+            		return HttpResponseRedirect('/')
+    	else:
+    		form = SignUpForm()
 
-    return render(request, 'registration.html', {
+    	return render(request, 'registration.html', {
 		'form' : form,
 	})  
 
@@ -102,30 +103,15 @@ def user_login(request):
 	if request.method == "POST":
 		form = LoginForm(request.POST)
 		username = request.POST.get('username')
-        password = request.POST.get('password')
-        if form.is_valid():
-        	user = authenticate(username=username, password=password)
-        	if user is not None:
-        		login(request, user)
-            	return HttpResponseRedirect('/')
-    else:
-    	form = LoginForm()
+        	password = request.POST.get('password')
+        	if form.is_valid():
+        		user = authenticate(username=username, password=password)
+        		if user is not None:
+        			login(request, user)
+            			return HttpResponseRedirect('/')
+    	else:
+    		form = LoginForm()
 
-    return render(request, 'login.html', {
+    	return render(request, 'login.html', {
 		'form' : form,
 	})  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
